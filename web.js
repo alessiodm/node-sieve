1d784dc2ac6aa016620d8ff72c958b22bb41b459
Alessio Della Motta <alessio@thousandeyes.com>
2013-09-17 22:32:57 +0200

    Removed dependencies

--- web.js
+++ web.js
@@ -1,18 +1,13 @@
-var express = require('express');
+var http = require('http');
 var sieve = require('./sieve.js');
 
-var app = express.createServer(express.logger());
-
-app.get('/:num?', function(request, response) {
-  var range = 100 + Math.floor(Math.random() * 100);
-  var num = parseInt(request.params.num);
-  if (typeof num === 'number' && num !== NaN && num > 0 && num < 10000000){
-  	range = num;
-  }
-  response.send(sieve.eratosthenes(range));
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+
+http.createServer(function (req, res) {
+  var range = 100000 + Math.floor(Math.random() * 1000);
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(sieve.eratosthenes(range).join(", "));
+}).listen(port, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:' + port);
